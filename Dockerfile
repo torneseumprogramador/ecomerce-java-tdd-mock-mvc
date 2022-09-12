fde3e720e04f74cad39db810c06a2e5fcb393350
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-09-12 09:33:41 -0300

    publica no docker hub

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM openjdk:18-jdk-alpine
+COPY target/*.jar app.jar
+ENTRYPOINT ["java","-jar","/app.jar"]
---
+++ cria_imagem_docker_e_publica.sh
@@ -0,0 +1,11 @@
+mvn clean
+mvn package
+
+echo "Digite a tag para publicação"
+read TAG
+
+git tag $TAG
+git push --tags
+
+docker build -t didox/ecommerce-treinamento-ibm:$TAG .
+docker push didox/ecommerce-treinamento-ibm:$TAG
