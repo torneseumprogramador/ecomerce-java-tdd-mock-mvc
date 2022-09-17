636fe586ce999ee39de2d39cf0f15d5de77d180c
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-09-17 13:33:36 -0300

    dados turma2

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,4 +1,4 @@
 git checkout .
 git pull --force
-sed -i '' -e "s/IMG_PARA_SUBSTITUIR/didox\/turma1_ibm_java_ecommerce_k8s:$TAG/" deployment-ecommerce-pipeline.yml
+sed -i '' -e "s/IMG_PARA_SUBSTITUIR/didox\/k8s_ecommerce:$TAG/" deployment-ecommerce-pipeline.yml
 kubectl apply -f deployment-ecommerce-pipeline.yml
