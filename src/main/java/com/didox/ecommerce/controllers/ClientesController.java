d44fc39cfcef856af7c04208d3a83ee84edb306f
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-09-27 22:12:45 -0300

    librando cross domain

--- src/main/java/com/didox/ecommerce/controllers/ClientesController.java
+++ src/main/java/com/didox/ecommerce/controllers/ClientesController.java
@@ -2,6 +2,7 @@ package com.didox.ecommerce.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.didox.ecommerce.daos.IClientesDao;
 import com.didox.ecommerce.models.Cliente;
 
+@CrossOrigin("*")
 @RestController
 public class ClientesController {
 
